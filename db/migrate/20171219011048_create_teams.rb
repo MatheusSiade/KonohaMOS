10927088e066cded23e958135735c17127f06661
matheussiade <matheussiadef@outlook.com>
2017-12-19 12:51:48 -0200

    Criação do model de times Tentativa inicial de aplicação do materialize

---
+++ db/migrate/20171219011048_create_teams.rb
@@ -0,0 +1,13 @@
+class CreateTeams < ActiveRecord::Migration[5.1]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :person1_id
+      t.string :person2_id
+      t.string :person3_id
+      t.string :personsuperior_id
+
+      t.timestamps
+    end
+  end
+end
